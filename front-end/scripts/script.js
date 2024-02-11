34ff0b53a6084b7e858fa79f895c7352c44f439b
priyamenezes <113142516+pmenezes3094@users.noreply.github.com>
2024-02-10 23:36:13 -0800

    add-thought-bar

--- front-end/scripts/script.js
+++ front-end/scripts/script.js
@@ -1,6 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     var button = document.querySelector('.search');
-    var content = document.querySelector('.slide-bar');
+    var content = document.querySelector('.search-bar');
+
+    button.addEventListener('click', function() {
+        content.classList.toggle('active');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.querySelector('.add');
+    var content = document.querySelector('.add-bar');
 
     button.addEventListener('click', function() {
         content.classList.toggle('active');
