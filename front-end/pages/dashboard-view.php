3d5450b371963780e2c89806014b06fbc3331089
priyamenezes <113142516+pmenezes3094@users.noreply.github.com>
2024-02-15 02:16:30 -0800

    delete a record

--- front-end/pages/dashboard-view.php
+++ front-end/pages/dashboard-view.php
@@ -2,8 +2,8 @@
 require '../../back-end/pages/error.php';
 require '../../back-end/database.php';
 require '../../back-end/pages/session-variables-set.php';
-require '../../back-end/pages/dashboard.php'; 
-require 'partials/head.php'; 
+require '../../back-end/pages/dashboard.php';
+require 'partials/head.php';
 ?>
 
 <?php require 'partials/header.php'; ?>
@@ -15,6 +15,21 @@ require 'partials/head.php';
         </div>
 
         <div class="card-container">
+            <div class="grid-card">
+                <div class="data-content">
+                    <p>hHi My name is priya and I love design. this is continue flow of content that I am testing right now. I'm trying to write more content to test more contest sorry</p>
+                    <img src="../assets/images/background.jpg">
+                </div>
+                <div class="card-content">
+                    <button class="slide-button button-icon">
+                        <img src="../assets/icons/edit.svg" alt="">
+                    </button>
+                    <button class="slide-button button-icon">
+                        <img src="../assets/icons/delete.svg" alt="">
+                    </button>
+                </div>
+            </div>
+
             <?php foreach ($cards_data as $card) : ?>
                 <div class="grid-card">
                     <div class="data-content">
@@ -24,13 +39,18 @@ require 'partials/head.php';
                         <button class="slide-button button-icon">
                             <img src="../assets/icons/edit.svg" alt="Edit">
                         </button>
-                        <button class="slide-button button-icon">
-                            <img src="../assets/icons/delete.svg" alt="Delete">
-                        </button>
+                        <form action="../../back-end/pages/delete.php" method="post">
+                            <input type="hidden" name="data_id" value="<?php echo $card['data_id']; ?>">
+                            <button type="submit" class="slide-button button-icon" name="delete">
+                                <img src="../assets/icons/delete.svg" alt="Delete">
+                            </button>
+                        </form>
                     </div>
                 </div>
             <?php endforeach; ?>
         </div>
+
+
     </div>
 </main>
 
